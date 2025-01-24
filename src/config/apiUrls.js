ccc61802f7595563710a25752119bb5f277d2b3e
Nhattruong <nhattr2306@gmail.com>
2025-01-24 14:49:51 +0700

    update code

--- src/config/apiUrls.js
+++ src/config/apiUrls.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3333";
-// export const API_URL = "https://demo-1wls.onrender.com";
+// export const API_URL = "http://localhost:3333";
+export const API_URL = "https://demo-1wls.onrender.com";
