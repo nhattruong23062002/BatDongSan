1f96cec0a3e48daf8c86f31bb1967a421fb6c9c1
tranconglai01062000 <tranlai.0106@gmail.com>
2025-01-25 09:10:58 +0700

    codecuoinam

--- src/App.js
+++ src/App.js
@@ -14,32 +14,33 @@ import HomePage from "./pages/homePage";
 import Dashboard from "./pages/DashBoard";
 import SearchResults from "./pages/SearchResult";
 import FavoriteProperty from "./pages/FavoriteProperty";
-import "./App.css"
+import "./App.css";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route element={<LayoutUser />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/detail/:id" element={<PropertyDetail />} />
-          <Route path="/map" element={<MapPage />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/search" element={<SearchResults />} />
-          <Route path="/favorite" element={<FavoriteProperty />} />
-        </Route>
+    return (
+        <Router>
+            <Routes>
+                <Route element={<LayoutUser />}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/detail/:id" element={<PropertyDetail />} />
+                    <Route path="/map/:category" element={<MapPage />} />
+                    <Route path="/map" element={<MapPage />} />
+                    <Route path="/register" element={<RegisterForm />} />
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/search" element={<SearchResults />} />
+                    <Route path="/favorite" element={<FavoriteProperty />} />
+                </Route>
 
-        <Route path="/admin" element={<LayoutAdmin />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="manageUser" element={<ManageUser />} />
-          <Route path="manageProperty" element={<ManageProperty />} />
-          <Route path="createProperty" element={<CreateProperty />} />
-          <Route path="editProperty/:id" element={<EditProperty />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+                <Route path="/admin" element={<LayoutAdmin />}>
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="manageUser" element={<ManageUser />} />
+                    <Route path="manageProperty" element={<ManageProperty />} />
+                    <Route path="createProperty" element={<CreateProperty />} />
+                    <Route path="editProperty/:id" element={<EditProperty />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
